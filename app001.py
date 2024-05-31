72670c39982c895a79529162d65c3ac73982a984
rudyluis <rudy.manzaneda@gmail.com>
2024-05-30 21:50:09 -0400

    Subida y actuliazacion

---
+++ app001.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import datetime
+
+# Inyectar CSS personalizado para cambiar el color de la fuente en la barra lateral
+st.markdown(
+    """
+    <style>
+    .backgroundcolor {
+        color: black;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.title('Demostración de Widgets de Streamlit')
+
+# Métricas superiores
+num_varones = 10  # Ejemplo de número de varones, ajustar según datos reales
+num_mujeres = 15  # Ejemplo de número de mujeres, ajustar según datos reales
+edad_promedio_varones = 30  # Ejemplo de edad promedio, ajustar según datos reales
+edad_promedio_mujeres = 28  # Ejemplo de edad promedio, ajustar según datos reales
+
+col1, col2 = st.columns(2)
+col1.metric(label="👦 Varones", value=num_varones, delta=f"Edad Promedio: {edad_promedio_varones} años")
+col2.metric(label="👧 Mujeres", value=num_mujeres, delta=f"Edad Promedio: {edad_promedio_mujeres} años")
+
+
+
+# Botón en la barra lateral
+if st.sidebar.button('Saludar'):
+    st.write('¡Hola!')
+
+# Checkbox en la barra lateral
+acuerdo = st.sidebar.checkbox('Estoy de acuerdo')
+if acuerdo:
+    st.write('¡Genial!')
+
+# Radio Button en la barra lateral
+genero = st.sidebar.radio(
+    "¿Cuál es tu género de película favorito?",
+    ('Comedia', 'Drama', 'Documental'))
+if genero:
+    st.write('Has seleccionado:', genero)
+
+# Selectbox en la barra lateral
+metodo_contacto = st.sidebar.selectbox(
+    '¿Cómo te gustaría ser contactado?',
+    ('Correo electrónico', 'Teléfono fijo', 'Teléfono móvil'))
+st.write('Has seleccionado:', metodo_contacto)
+
+# Multiselect en la barra lateral
+colores = st.sidebar.multiselect(
+    '¿Cuáles son tus colores favoritos?',
+    ['Verde', 'Amarillo', 'Rojo', 'Azul'],
+    ['Amarillo', 'Rojo'])
+st.write('Has seleccionado:', colores)
+
+# Slider en la barra lateral
+edad = st.sidebar.slider('¿Cuántos años tienes?', 0, 130, 25)
+st.write("Tengo ", edad, 'años')
+
+# Text Input en la barra lateral
+titulo = st.sidebar.text_input('Título de la película', 'Titanic')
+st.write('El título de la película actual es', titulo)
+
+# Date Input en la barra lateral
+fecha = st.sidebar.date_input("¿Cuándo es tu cumpleaños?", datetime.date(2019, 7, 6))
+st.write('Tu cumpleaños es:', fecha)
+
+# File Uploader en la barra lateral
+archivo_subido = st.sidebar.file_uploader("Elige un archivo")
+if archivo_subido is not None:
+    df = pd.read_csv(archivo_subido)
+    st.write(df)
+
+# Color Picker en la barra lateral
+color = st.sidebar.color_picker('Elige un color', '#00f900')
+st.write('El color actual es', color)
---
+++ app003.py
@@ -0,0 +1,81 @@
+import altair as alt
+import streamlit as st
+from vega_datasets import data
+
+st.set_page_config(layout="wide")
+
+# Datos
+df = data.cars()
+
+st.write(df)
+item_list = [
+    col for col in df.columns if df[col].dtype in ['float64', 'int64']]
+origin_list = list(df['Origin'].unique())
+
+df['YYYY'] = df['Year'].apply(lambda x: x.year)
+min_year = df['YYYY'].min().item()
+max_year = df['YYYY'].max().item()
+
+# Diseño (Barra Lateral)
+st.sidebar.title("Dashboard de Registros de Autos")
+st.sidebar.markdown('###')
+st.sidebar.markdown("### *Configuraciones*")
+start_year, end_year = st.sidebar.slider(
+    "Periodo",
+    min_value=min_year, max_value=max_year,
+    value=(min_year, max_year))
+
+st.sidebar.markdown('###')
+origins = st.sidebar.multiselect('Orígenes', origin_list,
+                                 default=origin_list)
+st.sidebar.markdown('###')
+item1 = st.sidebar.selectbox('Elemento 1', item_list, index=0)
+item2 = st.sidebar.selectbox('Elemento 2', item_list, index=3)
+
+df_rng = df[(df['YYYY'] >= start_year) & (df['YYYY'] <= end_year)]
+source = df_rng[df_rng['Origin'].isin(origins)]
+
+# Contenido
+base = alt.Chart(source).properties(height=300)
+
+bar = base.mark_bar().encode(
+    x=alt.X('count(Origin):Q', title='Número de Registros'),
+    y=alt.Y('Origin:N', title='Origen'),
+    color=alt.Color('Origin:N', legend=None)
+)
+
+point = base.mark_circle(size=50).encode(
+    x=alt.X(item1 + ':Q', title=item1),
+    y=alt.Y(item2 + ':Q', title=item2),
+    color=alt.Color('Origin:N', title='',
+                    legend=alt.Legend(orient='bottom-left'))
+)
+
+line1 = base.mark_line(size=5).encode(
+    x=alt.X('yearmonth(Year):T', title='Fecha'),
+    y=alt.Y('mean(' + item1 + '):Q', title=item1),
+    color=alt.Color('Origin:N', title='',
+                    legend=alt.Legend(orient='bottom-left'))
+)
+
+line2 = base.mark_line(size=5).encode(
+    x=alt.X('yearmonth(Year):T', title='Fecha'),
+    y=alt.Y('mean(' + item2 + '):Q', title=item2),
+    color=alt.Color('Origin:N', title='',
+                    legend=alt.Legend(orient='bottom-left'))
+)
+
+# Diseño (Contenido)
+left_column, right_column = st.columns(2)
+
+left_column.markdown(
+    '**Número de Registros (' + str(start_year) + '-' + str(end_year) + ')**')
+right_column.markdown(
+    '**Diagrama de Dispersión de _' + item1 + '_ y _' + item2 + '_**')
+left_column.altair_chart(bar, use_container_width=True)
+right_column.altair_chart(point, use_container_width=True)
+
+left_column.markdown('**_' + item1 + '_ (Promedio Mensual)**')
+right_column.markdown('**_' + item2 + '_ (Promedio Mensual)**')
+left_column.altair_chart(line1, use_container_width=True)
+right_column.altair_chart(line2, use_container_width=True)
